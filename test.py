be00f935348af7449d1826850e7cc82f61412006
ut9n0k <86070068+ut9n0k@users.noreply.github.com>
2021-11-11 08:39:25 +0200

    Create test.py
    test task for teamcity-server

---
+++ test.py
@@ -0,0 +1,21 @@
+def amount(a: int, b: int):
+    return a + b
+
+
+def multiply(a: int, b: int):
+    return a * b
+
+
+def divide(a: int, b: int):
+    return a / b
+
+
+def subtract(a: int, b: int):
+    return a - b
+
+
+if __name__ == "__main__":
+    print(amount(10, 2))
+    print(multiply(10, 2))
+    print(divide(10, 2))
+    print(subtract(10, 2))
